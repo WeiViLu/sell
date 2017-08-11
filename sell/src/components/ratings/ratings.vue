e3f7a834eead87f9115cba0123ede2f262c6f293
陆薇薇 <WeiViLu@outlook.com>
2017-08-11 20:27:35 +0800

    更新

---
+++ sell/src/components/ratings/ratings.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		I'm ratings!
+	</div>
+</template>
+<script>
+	
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+	
+</style>
--- sell/src/main.js
+++ sell/src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import './common/stylus/index.styl'
 
 Vue.config.productionTip = false
+Vue.use(VueResource)
+Vue.use(VueRouter)
+
+router.push("/goods")
 
 /* eslint-disable no-new */
 new Vue({
