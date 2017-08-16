b4f2e1bd0cab1e289ed21d2fd54efbb8a40e5c62
陆薇薇 <WeiViLu@outlook.com>
2017-08-16 11:17:38 +0800

    第六次更新--shopcart组件完成

--- sell/src/components/shopcart/shopcart.vue
+++ sell/src/components/shopcart/shopcart.vue
@@ -3,17 +3,17 @@
 		<div class="content">
 			<div class="content-left">
 				<div class="logo-wrapper">
-					<div class="logo">
-						<span class="icon-shopping_cart"></span>
+					<div class="logo" :class="{'highlight':totalCount>0}">
+						<span class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></span>
 					</div>
-					<div class="num">{{totalCount}}</div>
+					<div class="num" v-show="totalCount>0">{{totalCount}}</div>
 				</div>
-				<div class="price">¥{{totalPrice}}元</div>
+				<div class="price" :class="{'highlight':totalPrice>0}">¥{{totalPrice}}元</div>
 				<div class="desc">另需配送费￥{{deliveryPrice}}元</div>
 			</div>
 			<div class="content-right">
-				<div class="pay">
-					¥{{minPrice}}元起送
+				<div class="pay" :class="payClass">
+					{{payDesc}}
 				</div>
 			</div>
 		</div>
@@ -56,6 +56,23 @@
 					count+=food.count;
 				})
 				return count;
+			},
+			payDesc(){
+				if(this.totalPrice===0){
+					return `¥${this.minPrice}元起送`;
+				}else if(this.totalPrice<this.minPrice){
+					let diff=this.minPrice-this.totalPrice;
+					return 	`还差¥${diff}元起送`
+				}else{
+					return '去结算';
+				}
+			},
+			payClass(){
+				if(this.totalPrice<this.minPrice){
+					return 'not-enough';
+				}else{
+					return 'enough';
+				}
 			}
 		}
 	}
@@ -92,10 +109,28 @@
 						border-radius:50%
 						background:#2b343c
 						text-align:center
+						&.highlight
+							background:rgb(0,160,220)
 						.icon-shopping_cart
 							line-height:44px
 							font-size:24px
 							color:#80858a
+							&.highlight
+								color:#fff 
+					.num
+						position:absolute
+						top:0
+						right:0
+						width:24px
+						height:16px
+						line-height:16px
+						text-align:center
+						border-radius:16px
+						font-size:9px
+						font-weight:700
+						color:#fff
+						background:rgb(240,20,20)
+						box-shadow:0 4px 8px rgba(0,0,0,0.4)
 				.price
 					display:inline-block
 					vertical-align:top
@@ -106,6 +141,8 @@
 					box-sizing:border-box
 					font-size:16px
 					font-weight:700
+					&.highlight
+						color:#fff
 				.desc
 					display:inline-block
 					vertical-align:top
@@ -122,4 +159,9 @@
 					font-size:12px
 					font-weight:700
 					background:#2b333b
+					&.not-enough
+						background:#2b333b
+					&.enough
+						background:#00b43c
+						color:#fff
 </style>
