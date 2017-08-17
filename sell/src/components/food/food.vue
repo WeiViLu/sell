599b0191d6c1c29f965269fd52d43fc5ee247e2d
陆薇薇 <WeiViLu@outlook.com>
2017-08-17 17:58:49 +0800

    第十一次更新--split组件实现

--- sell/src/components/food/food.vue
+++ sell/src/components/food/food.vue
@@ -17,13 +17,18 @@
 					<div class="price">
 						<span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
 					</div>
+					<div class="cartcontrol-wrapper">
+						<cartcontrol :food="food"></cartcontrol>
+					</div>
+					<transition name="fade">
+						<div @click.stop.prevent="addFirst" class="buy" v-show="!food.count||food.count===0">加入购物车</div>
+					</transition>
 				</div>
-				<div class="cartcontrol-wrapper">
-					<cartcontrol :food="food"></cartcontrol>
+				<split v-show="food.info"></split>	
+				<div class="info" v-show="food.info">
+					<h1 class="title">商品消息</h1>
+					<p class="text">{{food.info}}</p>
 				</div>
-				<transition name="fade">
-					<div @click.stop.prevent="addFirst" class="buy" v-show="!food.count||food.count===0">加入购物车</div>
-				</transition>
 			</div>
 		</div>
 	</transition>
@@ -32,7 +37,7 @@
 	import Vue from 'vue';
 	import BScroll from 'better-scroll';
 	import cartcontrol from '../cartcontrol/cartcontrol';
-
+	import split from '../split/split';
 	export default{
 		props:{
 			food:{
@@ -69,7 +74,8 @@
 			}
 		},
 		components:{
-			cartcontrol
+			cartcontrol,
+			split
 		}
 	}
 </script>
@@ -109,6 +115,7 @@
 					color:#fff
 		.content
 			padding:18px
+			position:relative
 			.title
 				line-height:14px
 				margin-bottom:8px
@@ -135,26 +142,38 @@
 					text-decoration:line-through
 					font-size:10px
 					color:rgb(147,153,159)
-		.cartcontrol-wrapper
-			position:absolute
-			right:12px
-			bottom:12px
-		.buy
-			position:absolute
-			right:18px
-			bottom:18px
-			z-index:10
-			height:24px
-			line-height:24px
-			padding:0 12px
-			box-sizing:border-box
-			font-size:10px
-			border-radius:12px
-			color:#fff
-			background:rgb(0,160,220)
-			opaciy:1
-			&.fade-enter-active,&.fade-leave-active
-				transition:all 0.2s
-			&.fade-enter,&.fade-leave-active
-				opaciy:0
+			.cartcontrol-wrapper
+				position:absolute
+				right:12px
+				bottom:12px
+			.buy
+				position:absolute
+				right:18px
+				bottom:18px
+				z-index:10
+				height:24px
+				line-height:24px
+				padding:0 12px
+				box-sizing:border-box
+				font-size:10px
+				border-radius:12px
+				color:#fff
+				background:rgb(0,160,220)
+				opaciy:1
+				&.fade-enter-active,&.fade-leave-active
+					transition:all 0.2s
+				&.fade-enter,&.fade-leave-active
+					opaciy:0
+		.info
+			padding:18px
+			.title
+				line-height:14px
+				margin-bottom:6px
+				font-size:14px
+				color:rgb(7,17,27)
+			.text
+				line-height:24px
+				padding:0 8px
+				font-size:12px
+				color:rgb(77,85,93)
 </style>
