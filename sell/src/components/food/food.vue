ed5712311310099bc12144821837f2297c266806
陆薇薇 <WeiViLu@outlook.com>
2017-08-18 18:02:18 +0800

    第十二次更新--评价列表未完成

--- sell/src/components/food/food.vue
+++ sell/src/components/food/food.vue
@@ -33,6 +33,22 @@
 				<div class="rating">
 					<h1 class="title">商品评价</h1>
 					<ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+					<div class="rating-wrapper">
+						<ul v-show="food.ratings && food.ratings.length">
+							<li v-for="rating in food.ratings" class="rating-item border-1px
+							" v-show="needShow(rating.rateType,rating.text)">
+								<div class="user">
+									<span class="name">{{rating.username}}</span>
+									<img :src="rating.avatar" class="avatar" width="12" height="12">
+								</div>
+								<div class="time">{{rating.rateTime}}</div>
+								<p class="text">
+									<span :class="{'icon-thumb_up' :rating.rateType===0,'icon-thumb_down' :rating.rateType===1}"></span>{{rating.text}}
+								</p>
+							</li>
+						</ul>
+						<div class="no-rating" v-show="!food.ratings||!food.ratings.length"></div>	
+					</div>
 				</div>
 			</div>
 		</div>
@@ -69,7 +85,7 @@
 			show(){
 				this.showFlag=true;
 				this.selectType=ALL;
-				this.onlyContent=false;
+				this.onlyContent=true;
 				this.$nextTick((food)=>{
 					if(!this.scroll){
 						this.scroll=new BScroll(this.$refs.food,{
@@ -89,6 +105,16 @@
 				}
 				this.$emit('add',event.target);
 				Vue.set(this.food,'count',1);
+			},
+			needShow(type,text){
+				if(this.onlyContent && !text){
+					return false;
+				}
+				if(this.selectType===ALL){
+					return true;
+				}else{
+					return type===this.selectType;
+				}
 			}
 		},
 		components:{
@@ -99,7 +125,7 @@
 	}
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
-
+	@import "../../common/stylus/mixin"
 	.food
 		position:fixed
 		left:0
@@ -202,5 +228,42 @@
 				line-height:14px
 				margin-left:18px
 				font-size:14px
-				color:rgb(7,17,27) 	
+				color:rgb(7,17,27)
+			.rating-wrapper
+				padding:0 18px
+				.rating-item
+					position:relative
+					padding:16px 0
+					border-1px(rgba(7,17,27,0.2))
+					.user
+						position:absolute
+						right:0
+						top:16px
+						line-height:12px
+						font-size:0
+						.name
+							display:inline-block
+							vertical-align:top
+							margin-right:6px
+							font-size:10px
+							color:rgb(147,153,159)
+						.avatar    
+							border-radius:50%
+					.time
+						margin-bottom:6px
+						line-height:12px
+						font-size:10px
+						color:rgb(147,153,159)
+					.text
+						line-height:16px
+						font-size:12px
+						color:rgb(7,17,27)
+						.icon-thumb_up,.icon-thumb_down
+							line-height:16px
+							margin-right:4px
+							font-size:12px
+						.icon-thumb_up
+							color:rgb(0,160,220)
+						.icon-thumb_down
+							color:rgb(147,153,159)
 </style>
