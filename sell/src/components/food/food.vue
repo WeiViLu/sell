1eef0df46f7198493780e84154f78245d8dfc150
陆薇薇 <WeiViLu@outlook.com>
2017-08-18 15:22:06 +0800

    第十一次更新--ratingselect组件完成

--- sell/src/components/food/food.vue
+++ sell/src/components/food/food.vue
@@ -29,6 +29,11 @@
 					<h1 class="title">商品消息</h1>
 					<p class="text">{{food.info}}</p>
 				</div>
+				<split></split>
+				<div class="rating">
+					<h1 class="title">商品评价</h1>
+					<ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+				</div>
 			</div>
 		</div>
 	</transition>
@@ -38,6 +43,10 @@
 	import BScroll from 'better-scroll';
 	import cartcontrol from '../cartcontrol/cartcontrol';
 	import split from '../split/split';
+	import ratingselect from '../ratingselect/ratingselect'
+	const POSITIVE=0;
+	const NEGATIVE=1;
+	const ALL=2;
 	export default{
 		props:{
 			food:{
@@ -46,12 +55,21 @@
 		},
 		data(){
 			return{
-				showFlag:false
+				showFlag:false,
+				selectType:ALL,
+				onlyContent:true,
+				desc:{
+					all:'全部',
+					positive:'推荐',
+					negative:'吐槽'
+				}
 			}
 		},
 		methods:{
 			show(){
 				this.showFlag=true;
+				this.selectType=ALL;
+				this.onlyContent=false;
 				this.$nextTick((food)=>{
 					if(!this.scroll){
 						this.scroll=new BScroll(this.$refs.food,{
@@ -75,11 +93,13 @@
 		},
 		components:{
 			cartcontrol,
-			split
+			split,
+			ratingselect
 		}
 	}
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
+
 	.food
 		position:fixed
 		left:0
@@ -176,4 +196,11 @@
 				padding:0 8px
 				font-size:12px
 				color:rgb(77,85,93)
+		.rating
+			padding-top:18px
+			.title
+				line-height:14px
+				margin-left:18px
+				font-size:14px
+				color:rgb(7,17,27) 	
 </style>
