3e6cae5106d7a4f1ebd21b89bf4c2d005f773596
lww <WeiViLu@outlook.com>
2017-08-21 07:20:59 +0800

    第十八次更新--商家实景完成

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -42,6 +42,17 @@
 					</li>
 				</ul>
 			</div>
+			<split></split>
+			<div class="pics">
+				<h1 class="title">商家实景</h1>
+				<div class="pic-wrapper" ref="picWrapper">
+					<ul class="pic-list" ref="picList">
+						<li class="pic-item" v-for="pic in seller.pics">
+							<img :src="pic" alt="" width="120" height="120">
+						</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -62,6 +73,23 @@
 			this.scroll=new BScroll(this.$refs.seller,{
 				click:true
 			})
+			if(this.seller.pics){
+				let picWidth=120;
+				let margin=6;
+				let width=(picWidth+margin)*this.seller.pics.length-margin;
+				this.$refs.picList.style.width=width+'px';
+				this.$nextTick(()=>{
+					if(!this.picScorll){
+						this.picScorll=new BScroll(this.$refs.picWrapper,{
+							scrollX:true,
+							eventPasssThrough:'vertical'
+						})
+					}else{
+						this.picScorll.refresh();
+					}
+					
+				})
+			}
 		},
 		components:{
 			star,
@@ -139,6 +167,8 @@
 					padding:16px 12px
 					font-size:0
 					border-1px(rgba(7,17,27,0.1))
+					&:last-child
+						border-none()
 				.icon
 					display:inline-block
 					vertical-align:top
@@ -160,5 +190,25 @@
 				.text
 					line-height:16px
 					font-size:12px
-					color:rgb(7,17,27)
+					color:rgb(7,17,27)  
+		.pics
+			padding:18px
+			.title
+				margin-bottom:12px
+				line-height:14px
+				color:rgb(7,17,27)
+				font-size:14px
+			.pic-wrapper
+				width:100%
+				overflow:hidden
+				white-space:nowrap
+				.pic-list
+					font-size:0
+					.pic-item
+						display:inline-block
+						margin-right:6px
+						width:120px
+						height:90px
+						&:last-child
+							margin:0
 </style>
